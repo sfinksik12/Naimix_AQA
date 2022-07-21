993e38fbe06534df5da909a06c84aea7d472e197
User <vasya1pupkin12453@gmail.com>
2022-07-21 22:00:30 +0300

    1. Добавление метода POST в класс App 2. Изменение Data/users.py -> третье поле словаря "auth_token"

---
+++ Locators/element_create_user_form.py
@@ -0,0 +1,16 @@
+LASTNAME = '[name=lastName]'
+NAME = '[name=firstName]'
+SERNAME = '[name=patronymic]'
+
+POSITION = '[id=position]'
+ROLE = '[id=role]'
+
+PHONE = '[name=phone]'
+EMAIL = '[name=email]'
+
+PASSWORD = '[name=password]'
+REPEAT_PASSWORD = '[name=repeatPassword]'
+
+CANSEL = 'button.apply-buttons__cancel.nm-button.nm-button_color-white.nm-button_size-xl'
+ADD = 'button.apply-buttons__submit'
+
